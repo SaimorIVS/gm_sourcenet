c184341a2dd3c1ea72a76cb0c2b437a9fe549c3d
Mikkel Struntze <mikkel.struntze@gmail.com>
2023-04-13 00:11:14 +0200

    GA

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,11 @@
-PROJECT_GENERATOR_VERSION = 2
+PROJECT_GENERATOR_VERSION = 3
 
 newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
 	value = "path to garrysmod_common directory"
 })
-
+externalincludedirs = sysincludedirs
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
